216d632f53d0675c05c5b446172aa5e4429e06ca
Hasan Dababo <hasand34500@gmail.com>
2025-11-29 17:10:36 -0800

    Initial Commit

---
+++ TennisTournamentPredictorApp/TennisTournamentPredictorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisTournamentPredictorAppApp.swift
+//  TennisTournamentPredictorApp
+//
+//  Created by cisstudent on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TennisTournamentPredictorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
